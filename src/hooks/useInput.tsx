46a9ec5bb6f790ee00091856280fc5231c1f13d3
junni11 <wkszlf98@naver.com>
2021-07-21 05:14:13 +0900

    home화면에 상품 진열 추가 test로 8개

--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 
 /**
  * Input Tag에서 입력받은 값의 유효성을 검증
- * 값의 유효성 검사는 두번째 인자로 전달받은 함수로 함
- * 유효한 값이면 해당 값과 이벤트 핸들러를 반환!!!!!!!!!!!!!
+ * 값의 유효성 검사는 두번째 인자로 전달받은 함수로 함1!!!!!ss
+ * 유효한 값이면 해당 값과 이벤트 핸들러를 반환!!!!!!!!!!!!!!!!!!!!!!!s!!
  */
 const useInput = (initialValue: any, validator: (value: any) => boolean) => {
   const [value, setValue] = useState(initialValue);
