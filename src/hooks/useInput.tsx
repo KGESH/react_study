aa0d46db278c656a32d8e0ef80bf30ce6c104aaf
junni11 <wkszlf98@naver.com>
2021-07-21 05:00:30 +0900

    test2

--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 /**
  * Input Tag에서 입력받은 값의 유효성을 검증
  * 값의 유효성 검사는 두번째 인자로 전달받은 함수로 함
- * 유효한 값이면 해당 값과 이벤트 핸들러를 반환!!!!!
+ * 유효한 값이면 해당 값과 이벤트 핸들러를 반환!!!!!!!!!!!!!
  */
 const useInput = (initialValue: any, validator: (value: any) => boolean) => {
   const [value, setValue] = useState(initialValue);
